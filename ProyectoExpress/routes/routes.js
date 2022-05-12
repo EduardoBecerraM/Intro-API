dce20aecfbf08c52e6f52cdfd3a496daa4a004fd
Eduardo Becerra <beduardo.becerra19@gmail.com>
2022-05-12 11:28:54 -0600

    Se agrego una nueva ruta para actualizar un usuario existente

--- ProyectoExpress/routes/routes.js
+++ ProyectoExpress/routes/routes.js
@@ -36,4 +36,13 @@ const router = app => {
         });
     });
 
+    //Actualizar un usuario existente
+    app.put('/users/:id', (request, response) => {
+        const id = request.params.id;
+        pool.query('UPDATE users SET ? WHERE id = ?', [request.body, id], (error, result) => {
+            if (error) throw error;
+            response.send('User updated successfully.');
+        });
+    });
+
 }
