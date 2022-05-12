91fa106a24e69c9a122757bdd2530964a74440ff
Eduardo Becerra <beduardo.becerra19@gmail.com>
2022-05-12 11:33:06 -0600

    Se agrego una nueva ruta para eliminar un usuario y se exporto el router

--- ProyectoExpress/routes/routes.js
+++ ProyectoExpress/routes/routes.js
@@ -45,4 +45,14 @@ const router = app => {
         });
     });
 
+    app.delete('/users/:id', (request, response) => {
+        const id = request.params.id;
+        pool.query('DELETE FROM users WHERE id = ?', id, (error, result) => {
+            if (error) throw error;
+            response.send('User deleted.')
+        });
+    });
 }
+
+//Exportar el router
+module.exports = router;
